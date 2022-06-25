2db1e6629f2f739702bdfbce2515c1b7fae04e88
Maciej Hejduk <maciej.hejduk@linkfactory.dk>
2022-06-25 22:30:49 +0200

    Create User Entity and User schema in database

---
+++ src/Entity/User.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ApiResource]
+#[ORM\Entity]
+class User
+{
+    const ROLES = ['Teacher', 'Student'];
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private int $id;
+
+    #[ORM\Column]
+    #[Assert\NotBlank]
+    private string $firstName;
+
+    #[ORM\Column]
+    #[Assert\NotBlank]
+    private string $lastName;
+
+    #[ORM\Column]
+    #[Assert\Choice(choices: User::ROLES)]
+    private string $role;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+    public function getRole(): string
+    {
+        return $this->role;
+    }
+
+    public function setRole(string $role): void
+    {
+        $this->role = $role;
+    }
+
+}
