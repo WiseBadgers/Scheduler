1b716e0ed79b84f9ebaff7c2c164902d579189cf
Marek Bareła <marek.barela@wp.pl>
2022-07-01 12:52:23 +0200

    Recreate database migration files

--- README.md
+++ README.md
@@ -12,13 +12,13 @@
 1. Configure database:
    1. `sudo mysql` OR `mysql -u root -p`
    2. `CREATE DATABASE scheduler;`
-   3. `CREATE USER 'symfony'@'localhost' IDENTIFIED BY password;`
+   3. `CREATE USER 'symfony'@'localhost' IDENTIFIED BY 'password';`
    4. `GRANT ALL PRIVILEGES ON scheduler.* TO 'symfony'@'localhost';`
 2. Clone repository: `git clone git@github.com:WiseBadgers/Scheduler.git`
 3. Run `composer install` to install php dependencies
 4. Open project in IDE and configure `.env.local` file:
    1. Create `.env.local` file
-   2. Input this line into the `.env.local`: 
+   2. Input this line into the `.env.local`:
       `DATABASE_URL="mysql://symfony:password@127.0.0.1:3306/scheduler?serverVersion=5.7&charset=utf8mb4"`
 5. Go to project root: `cd scheduler`
 6. Run database migrations: `symfony console doc:mig:mig`
---
+++ migrations/Version20220701104940.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220701104940 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE course (id INT AUTO_INCREMENT NOT NULL, teacher_id INT DEFAULT NULL, semester_id INT DEFAULT NULL, subject_id INT DEFAULT NULL, school_class_id INT DEFAULT NULL, INDEX IDX_169E6FB941807E1D (teacher_id), INDEX IDX_169E6FB94A798B6F (semester_id), INDEX IDX_169E6FB923EDC87 (subject_id), INDEX IDX_169E6FB914463F54 (school_class_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE note (id INT AUTO_INCREMENT NOT NULL, student_id INT DEFAULT NULL, course_id INT DEFAULT NULL, value INT NOT NULL, INDEX IDX_CFBDFA14CB944F1A (student_id), INDEX IDX_CFBDFA14591CC992 (course_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE school_class (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE semester (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE subject (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, school_class_id INT DEFAULT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), INDEX IDX_8D93D64914463F54 (school_class_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE course ADD CONSTRAINT FK_169E6FB941807E1D FOREIGN KEY (teacher_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE course ADD CONSTRAINT FK_169E6FB94A798B6F FOREIGN KEY (semester_id) REFERENCES semester (id)');
+        $this->addSql('ALTER TABLE course ADD CONSTRAINT FK_169E6FB923EDC87 FOREIGN KEY (subject_id) REFERENCES subject (id)');
+        $this->addSql('ALTER TABLE course ADD CONSTRAINT FK_169E6FB914463F54 FOREIGN KEY (school_class_id) REFERENCES school_class (id)');
+        $this->addSql('ALTER TABLE note ADD CONSTRAINT FK_CFBDFA14CB944F1A FOREIGN KEY (student_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE note ADD CONSTRAINT FK_CFBDFA14591CC992 FOREIGN KEY (course_id) REFERENCES course (id)');
+        $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D64914463F54 FOREIGN KEY (school_class_id) REFERENCES school_class (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE note DROP FOREIGN KEY FK_CFBDFA14591CC992');
+        $this->addSql('ALTER TABLE course DROP FOREIGN KEY FK_169E6FB914463F54');
+        $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D64914463F54');
+        $this->addSql('ALTER TABLE course DROP FOREIGN KEY FK_169E6FB94A798B6F');
+        $this->addSql('ALTER TABLE course DROP FOREIGN KEY FK_169E6FB923EDC87');
+        $this->addSql('ALTER TABLE course DROP FOREIGN KEY FK_169E6FB941807E1D');
+        $this->addSql('ALTER TABLE note DROP FOREIGN KEY FK_CFBDFA14CB944F1A');
+        $this->addSql('DROP TABLE course');
+        $this->addSql('DROP TABLE note');
+        $this->addSql('DROP TABLE school_class');
+        $this->addSql('DROP TABLE semester');
+        $this->addSql('DROP TABLE subject');
+        $this->addSql('DROP TABLE user');
+    }
+}
